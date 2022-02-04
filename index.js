c3e5f06b39dac59bfe14eee51b1abe60a69c994f
B.D.Kim <byungdeuk.kim@gamil.com>
2022-02-04 15:51:40 +0900

    sign up

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://bibidi:kevin123@boilerplate.8omzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World! 새해 복 많이 받아라!')
+});
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    else return res.status(200).json({
+      success: true
+    });
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
